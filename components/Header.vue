e641010ce96833c20816ea3d2bd92f259edff562
kashipu <graficowm@gmail.com>
2021-10-12 01:31:15 -0500

    Add(+) Init project structure, create Header component and import Google fonts support

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="header__container container p-2">
+      <div class="row align-items-center">
+        <div class="col-md-8">
+          <img class="img-fluid" src="~/assets/images/logoblanco.png" alt="" />
+        </div>
+        <div class="col-md-4 d-flex justify-content-end">
+          <a class="btn btn-light px-5 text-orange">Registrarse</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+header {
+  background-color: #ff6600;
+}
+
+.text-orange {
+  color: #ff6600;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <h1>Hola mundo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
+0.
